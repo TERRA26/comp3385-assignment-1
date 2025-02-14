9faa35a5fe268d8f281bee050b19123ee9610520
Christophe Marcellus <christophe.marcellus2005@gmail.com>
2025-02-14 16:33:21 -0400

    Added styling and profile image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>Christophe Marcellus</h2>
-<p>Level 2 Computer Science Student.</p>
+<h2>Christophe Marcellus </h2>
+<p>Level 2 Computer Science Student</p>
+<img src="{{ url('/images/computer.jpg') }}" alt="Your alternate description" />
 @endsection
